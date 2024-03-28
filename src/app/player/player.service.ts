b19334c2d63a4dc544d12bc734dcf3b657aeeee2
samaslima <samaslimag@gmail.com>
2024-03-28 13:48:07 -0300

    create player service

---
+++ src/app/player/player.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { PlayerT } from "./player.type";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PlayerService {
+  private array = ["player 1", "player 2", "player 3"];
+  public players$ = new BehaviorSubject(this.array.map(this.createPlayer));
+
+  private createPlayer(name: string): PlayerT {
+    return {
+      id: crypto.randomUUID(),
+      name,
+    };
+  }
+
+  public newPlayer(name: string): void {
+    const player = this.createPlayer(name);
+    const listPlayers: PlayerT[] = [...this.players$.getValue(), player];
+
+    this.players$.next(listPlayers);
+  }
+}
