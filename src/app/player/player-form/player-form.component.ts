731d0e987a38b76da5f8fb81580caaa840a544b3
samaslima <samaslimag@gmail.com>
2024-03-28 13:50:16 -0300

    create player form component

---
+++ src/app/player/player-form/player-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostBinding } from "@angular/core";
+import { PlayerService } from "../player.service";
+import { FormControl, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-player-form",
+  templateUrl: "./player-form.component.html",
+})
+export class PlayerFormComponent {
+  @HostBinding("class") classes = "flex flex-col gap-2";
+
+  name = new FormControl("", [Validators.required, Validators.minLength(3)]);
+
+  constructor(private playerService: PlayerService) {}
+
+  public submitForm(): void {
+    if (this.name.valid) {
+      this.playerService.newPlayer(this.name.value as string);
+      this.name.reset();
+    } else {
+      this.name.markAsDirty();
+    }
+  }
+}
