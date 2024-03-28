44cb3c0bed83eeb850bf45180153a325791f2fa9
samaslima <samaslimag@gmail.com>
2024-03-28 13:50:53 -0300

    create player module

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { PlayerListComponent } from "./player-list/player-list.component";
+import { PlayerDetailsComponent } from "./player-details/player-details.component";
+import { PlayerFormComponent } from "./player-form/player-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+    PlayerListComponent,
+    PlayerDetailsComponent,
+    PlayerFormComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [PlayerListComponent, PlayerFormComponent],
 })
 export class PlayerModule {}
