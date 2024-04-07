67270352d6e4aa1879cdb84700d290854ff88448
samaslima <samaslimag@gmail.com>
2024-04-07 12:55:20 -0300

    add player routes

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+
 import { PlayerListComponent } from "./player-list/player-list.component";
 import { PlayerDetailsComponent } from "./player-details/player-details.component";
 import { PlayerFormComponent } from "./player-form/player-form.component";
-import { ReactiveFormsModule } from "@angular/forms";
+
+const playerRoutes: Routes = [
+  { path: "players", component: PlayerListComponent },
+  { path: "player-form", component: PlayerFormComponent },
+];
 
 @NgModule({
   declarations: [
@@ -11,7 +18,11 @@ import { ReactiveFormsModule } from "@angular/forms";
     PlayerDetailsComponent,
     PlayerFormComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(playerRoutes),
+  ],
   exports: [PlayerListComponent, PlayerFormComponent],
 })
 export class PlayerModule {}
