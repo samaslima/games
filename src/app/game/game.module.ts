d250cb64f027f09c36f17256bdb2d593ed1972fd
samaslima <samaslimag@gmail.com>
2024-04-07 12:54:54 -0300

    add game routes

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+
 import { GameDetailsComponent } from "./game-details/game-details.component";
 import { GameListComponent } from "./game-list/game-list.component";
 import { GameFormComponent } from "./game-form/game-form.component";
-import { ReactiveFormsModule } from "@angular/forms";
 import { IconsModule } from "../icons/icons.module";
+
+const gameRoutes: Routes = [
+  { path: "games", component: GameListComponent },
+  { path: "game-form", component: GameFormComponent },
+];
 
 @NgModule({
   declarations: [GameDetailsComponent, GameListComponent, GameFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, IconsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IconsModule,
+    RouterModule.forChild(gameRoutes),
+  ],
   exports: [GameListComponent, GameFormComponent],
 })
 export class GameModule {}
