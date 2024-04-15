aa4e6a01804ac55a0334b6788008c7df64535245
samaslima <samaslimag@gmail.com>
2024-04-15 18:20:15 -0300

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { GameModule } from "./game/game.module";
+import { PlayerModule } from "./player/player.module";
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/games", pathMatch: "full" },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GameModule],
+  imports: [
+    BrowserModule,
+    GameModule,
+    PlayerModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
