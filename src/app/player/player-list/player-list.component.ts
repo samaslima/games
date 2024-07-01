807a4ef910efac51a7680acf1765ee872584dc26
samaslima <samaslimag@gmail.com>
2024-07-01 10:57:01 -0300

    add http client in player component

--- src/app/player/player-list/player-list.component.ts
+++ src/app/player/player-list/player-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from "@angular/core";
+import { Component, HostBinding, OnDestroy, OnInit } from "@angular/core";
 import { PlayersT } from "../player.type";
 import { PlayerService } from "../player.service";
 
@@ -6,16 +6,29 @@ import { PlayerService } from "../player.service";
   selector: "app-player-list",
   templateUrl: "./player-list.component.html",
 })
-export class PlayerListComponent implements OnInit {
+export class PlayerListComponent implements OnInit, OnDestroy {
   @HostBinding("class") classes = "flex flex-col gap-5";
+
+  private eventSubscription: any;
 
   players: PlayersT = [];
 
   constructor(private playerService: PlayerService) {}
 
   ngOnInit(): void {
-    this.playerService.players$.subscribe(
-      (newList) => (this.players = newList),
+    this.getPlayers();
+    this.eventSubscription = this.playerService.deleteEvent.subscribe(() =>
+      this.getPlayers(),
     );
+  }
+
+  ngOnDestroy(): void {
+    this.eventSubscription.unsubscribe();
+  }
+
+  public getPlayers(): void {
+    this.playerService
+      .getPlayers()
+      .subscribe((newList) => (this.players = newList));
   }
 }
