c4d351eb67719ce0756923a06b0c6f8f13c673a8
samaslima <samaslimag@gmail.com>
2024-07-01 09:20:12 -0300

    add unsubscribe

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from "@angular/core";
+import { Component, HostBinding, OnDestroy, OnInit } from "@angular/core";
 import { GamesT } from "../game.types";
 import { GameService } from "../game.service";
 
@@ -6,7 +6,7 @@ import { GameService } from "../game.service";
   selector: "app-game-list",
   templateUrl: "./game-list.component.html",
 })
-export class GameListComponent implements OnInit {
+export class GameListComponent implements OnInit, OnDestroy {
   @HostBinding("class") classes = "flex flex-col gap-5";
 
   games: GamesT = [];
@@ -16,6 +16,10 @@ export class GameListComponent implements OnInit {
   ngOnInit(): void {
     this.getGames();
     this.gameService.deleteEvent.subscribe(() => this.getGames());
+  }
+
+  ngOnDestroy(): void {
+    this.gameService.deleteEvent.unsubscribe();
   }
 
   public getGames(): void {
